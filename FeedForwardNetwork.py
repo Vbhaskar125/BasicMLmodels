b1627d0cb2865f1861e1f12e910096df03e71bb9
vijaya bhaskar <acvbhaskar94@gmail.com>
2017-06-11 11:32:06 +0530

    Create FeedForwardNetwork.py

--- FeedForwardNetwork.py
+++ FeedForwardNetwork.py
@@ -2,7 +2,7 @@
 ## A simple feed forward network using tensorflow and some of its visualization tools
 ##Architecture
 ## 2 hidden layers 1 input and 1 output layers
-## input layer : 9 neurons corresponding to season, mnth,holiday,workingday, weathersit, temp, atemp, hum, windspeed
+## input layer : 10 neurons corresponding to season, mnth,holiday,weekday,workingday, weathersit, temp, atemp, hum, windspeed
 ##hidden layers with 5 and 3 neurons respectively
 ##output neuron. This is a regression type of problem where the output value predicts the answer "cnt" in the dataset.
 ####################################################################################################
